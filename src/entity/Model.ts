ccf272db9a76f97531456c7fa4d381a4bdbf628c
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-05 16:42:19 +0530

    Create post entity and refactor shared Model code

---
+++ src/entity/Model.ts
@@ -0,0 +1,35 @@
+import {
+  BaseEntity,
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+
+export default abstract class Model extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "uuid" })
+  uuid: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @BeforeInsert()
+  createUuid() {
+    this.uuid = uuidv4();
+  }
+
+  toJSON() {
+    return {
+      ...this,
+      id: undefined,
+    };
+  }
+}
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,19 +1,8 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  BaseEntity,
-  CreateDateColumn,
-  UpdateDateColumn,
-  BeforeInsert,
-} from "typeorm";
-import { v4 as uuidv4 } from "uuid";
+import { Entity, Column } from "typeorm";
+import Model from "./Model";
 
 @Entity("users")
-export class User extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
-
+export class User extends Model {
   @Column()
   name: string;
 
@@ -22,25 +11,4 @@ export class User extends BaseEntity {
 
   @Column()
   role: string;
-
-  @Column({ type: "uuid" })
-  uuid: string;
-
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @UpdateDateColumn()
-  updatedAt: Date;
-
-  @BeforeInsert()
-  createUuid() {
-    this.uuid = uuidv4();
-  }
-
-  toJSON() {
-    return {
-      ...this,
-      id: undefined,
-    };
-  }
 }
