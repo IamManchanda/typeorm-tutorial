be947c020d9623ea694e835df8d2ea04f9d407c4
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-01-05 15:40:15 +0530

    Delete or Find user endpoint

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import { User } from "./entity/User";
       return res.status(200).json(users);
     } catch (error) {
       console.log(error);
-      return res.status(500).json({ error: "Something went wrong!" });
+      return res.status(500).json({ error: "Something went wrong." });
     }
   });
 
@@ -26,7 +26,7 @@ import { User } from "./entity/User";
       return res.status(201).json(user);
     } catch (error) {
       console.log(error);
-      return res.status(500).json({ error: "Something went wrong!" });
+      return res.status(500).json({ error: "Something went wrong." });
     }
   });
 
@@ -44,10 +44,42 @@ import { User } from "./entity/User";
         return res.status(200).json(user);
       } catch (error) {
         console.log(error);
-        return res.status(500).json({ error: "Something went wrong!" });
+        return res.status(500).json({ error: "Something went wrong." });
       }
     },
   );
+
+  app.delete(
+    "/users/:uuid",
+    async function deleteUser(req: Request, res: Response) {
+      const { uuid } = req.params;
+      try {
+        let user;
+        try {
+          user = await User.findOneOrFail({ uuid });
+        } catch (error) {
+          console.log(error);
+          return res.status(404).json({ error: "User not found." });
+        }
+        await user.remove();
+        return res.status(200).json({ message: "User deleted successfully." });
+      } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Something went wrong." });
+      }
+    },
+  );
+
+  app.get("/users/:uuid", async function findUser(req: Request, res: Response) {
+    const { uuid } = req.params;
+    try {
+      const user = await User.findOneOrFail({ uuid });
+      return res.status(200).json(user);
+    } catch (error) {
+      console.log(error);
+      return res.status(404).json({ error: "User not found." });
+    }
+  });
 
   try {
     const connection = await createConnection();
